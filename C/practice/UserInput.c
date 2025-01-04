9ca667ca14025db2e271ae7e5227b048108525e8
Andrei Visan <andrei.m.visan@gmail.com>
2025-01-04 19:11:31 +0100

    Added user input

--- C/practice/UserInput.c
+++ C/practice/UserInput.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     char name[25];
@@ -6,7 +7,12 @@ int main() {
     
     printf("\nWhat is your name? ");
     // read input from user
-    scanf("%s", &name);
+    // scanf("%s", &name);
+    // if the user needs to enter a space, use fgets
+    // name - the var , 25 - the size of the var, stdin - the input source
+    fgets(name, 25, stdin);
+    // remove the newline character from the end of the string added by fgets
+    name[strlen(name) - 1] = '\0';
     
     printf("\nHow old are you? ");
     scanf("%d", &age);
