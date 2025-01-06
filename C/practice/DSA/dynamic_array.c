2b14ac4834156395623443260b6b53973c9617c7
Andrei Visan^ <andrei.m.visan@gmail.com>
2025-01-06 19:31:52 +0100

    Finished defining and adding data to a dynamic array

--- C/practice/DSA/dynamic_array.c
+++ C/practice/DSA/dynamic_array.c
@@ -15,19 +15,45 @@ DynamicArray *init() {
     return arr;
 }
 
-void add(DynamicArray *arr, int index, int val) {
-    if (index >= arr->capacity && arr->size == arr->capacity - 1) {
-        // allocate more memory
-        arr->data = malloc(2*arr->capacity);
-    } else if (arr->size < arr->capacity - 1) {
-        // throw an error
+void expand_arr(DynamicArray *arr) {
+    arr->capacity *= 2;
+    arr->data = realloc(arr->data, sizeof(int) * arr->capacity);
+}
+
+int add(DynamicArray *arr, int index, int val) {
+    if (index < 0 || index > arr->size) {
+        fprintf(stderr, "Array capacity overflow\n");
+        return 1;
+    }
+    if (arr->size == arr->capacity) {
+        expand_arr(arr);
+    }
+    for (int i = arr->size; i > index; i--) {
+        arr->data[i] = arr->data[i-1];
     }
     arr->data[index] = val;
+    arr->size += 1;
+    return 0;
+}
+
+void free_mem(DynamicArray *arr) {
+    free(arr->data);
+    free(arr);
+}
+
+void print_arr(DynamicArray *arr) {
+    for (int i = 0; i < arr->size; i++) {
+        printf("%d ", arr->data[i]);
+    }
+    printf("\n");
 }
 
 int main() {
     DynamicArray *arr = init();
-    free(arr->data);
-    free(arr);
+    add(arr, 0, 10);
+    add(arr, 1, 7);
+    add(arr, 1, 11);
+    print_arr(arr);
+    free_mem(arr);
     return 0;
 }
