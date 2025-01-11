2d39b1d89c8e692b44ff137a4edcfa08e3629e6d
Andrei Visan <andrei.m.visan@gmail.com>
2025-01-11 19:15:34 +0100

    Progress on the Dynamic Array

---
+++ C/practice/DSA/array_t.c
@@ -0,0 +1,63 @@
+#include "array_t.h"
+
+array_t *array_t_init() {
+    array_t *arr = malloc(sizeof(array_t));
+    if (!arr) {
+        return NULL;
+    }
+    arr->capacity = 8;
+    arr->data = malloc(sizeof(int) * arr->capacity);
+    arr->size = 0;
+    return arr;
+}
+
+int array_t_add(array_t *arr, int index, int val) {
+    if (index < 0 || index > arr->size) {
+        fprintf(stderr, "Array capacity overflow\n");
+        return 1;
+    }
+    if (arr->size == arr->capacity) {
+        array_t_expand(arr);
+    }
+    for (int i = arr->size; i > index; i--) {
+        arr->data[i] = arr->data[i-1];
+    }
+    arr->data[index] = val;
+    arr->size += 1;
+    return 0;
+}
+
+int array_t_delete(array_t *arr, int index) {
+    if (index < 0 || index >= arr->size) {
+        fprintf(stderr, "Index out of range\n");
+        return 1; 
+    }
+    for (int i = index; i < arr-> size - 1; i++) {
+        arr->data[i] = arr->data[i + 1];
+    } 
+    arr->size -= 1;
+    array_t_reduce(arr);
+    return 0;
+}
+
+void array_t_print(const array_t *arr) {
+    for (int i = 0; i < arr->size; i++) {
+        printf("%d ", arr->data[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    array_t *arr = array_t_init();
+    array_t_add(arr, 0, 10);
+    array_t_add(arr, 1, 7);
+    array_t_add(arr, 1, 11);
+    array_t_add(arr, 2, 16);
+    array_t_print(arr);
+    printf("array size: %d\n", arr->size);
+    array_t_delete(arr, 2);
+    printf("array size: %d\n", arr->size);
+    array_t_print(arr);
+    array_t_free(arr);
+    return 0;
+}
---
+++ C/practice/DSA/array_t.h
@@ -0,0 +1,62 @@
+#ifndef ARRAY_T_H // Include guard start
+#define ARRAY_T_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+ * Type definition for the dynamic array. 
+ */
+ typedef struct {
+    int *data;
+    int size;
+    int capacity;
+ } array_t;
+
+/*
+ * Free the dynamic array memory
+ */
+static inline void array_t_free(array_t *arr) {
+    free(arr->data);
+    free(arr);
+}
+
+/*
+ * Expand array capacity
+ */
+static inline void array_t_expand(array_t *arr) {
+    arr->capacity *= 2;
+    arr->data = realloc(arr->data, sizeof(int) * arr->capacity);
+}
+
+/*
+ * Reduce array capacity
+ */
+static inline void array_t_reduce(array_t *arr) {
+    if (arr->size <= arr->capacity / 4) {
+        arr->capacity = arr->capacity / 2;
+        arr->data = realloc(arr->data, sizeof(int) * arr->capacity);
+    }
+}
+
+/*
+ * Initialize the dynamic array
+ */
+array_t *array_t_init();
+
+/*
+ * Insert value at index
+ */
+int array_t_add(array_t *arr, int index, int value);
+
+/*
+ * Delete value at index
+ */
+int array_t_delete(array_t *arr, int index);
+
+/*
+ * Print the array
+ */
+void array_t_print(const array_t *arr);
+
+#endif
