a5769e4f753be5b6222b08cc6de452fd48aedd76
Andrei Visan^ <andrei.m.visan@gmail.com>
2025-01-21 15:59:50 +0100

    Started simple arithmetic compiler

--- C/practice/simple_arithmetic_compiler/arithmetic_compiler.c
+++ C/practice/simple_arithmetic_compiler/arithmetic_compiler.c
@@ -19,13 +19,13 @@ Token *tokenize(const char *input) {
     Token *tokens = malloc(256 * sizeof(Token));
     int index = 0;
     while (input) {
-        if (isdigit(input)) {
+        if (isdigit(*input)) {
             tokens[index].token_type = TOKEN_NUMBER;
             /*
              * strtol expects a char** for endptr.
              * If input is a char*, &input is char**
              */
-            tokens[index].value = strtol(input, &input, 10);
+            tokens[index].value = strtol(input, (char**)&input, 10);
             index++;
         } else {
             switch(*input) {
@@ -49,4 +49,32 @@ Token *tokenize(const char *input) {
     return tokens;
 }
 
-//int parse 
+int parse(Token *tokens, int *result) {
+    int index = 0;
+    if (tokens[index].token_type != TOKEN_NUMBER) {
+        return 0; // Invalid expression
+    }
+    *result = tokens[index].value;
+    index++;
+    while (tokens[index].token_type != TOKEN_END) {
+        if (tokens[index].token_type == TOKEN_PLUS) {
+            index++;
+            if (tokens[index].token_type != TOKEN_NUMBER) {
+                return 0; // Invalid expression
+            }
+            *result += tokens[index].value;
+        } else if (tokens[index].token_type == TOKEN_MINUS) {
+            index++;
+            if (tokens[index].token_type != TOKEN_NUMBER) {
+                return 0; // Invalid expression
+            }
+            *result -= tokens[index].value; 
+        }
+        index++;
+    }
+    return 1;
+}
+
+int main() {
+    return 0;
+}
