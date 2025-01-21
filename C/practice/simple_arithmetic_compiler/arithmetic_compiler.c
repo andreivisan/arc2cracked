dfa2528dd4ce776b03a6101bcf3a470898871cee
Andrei Visan^ <andrei.m.visan@gmail.com>
2025-01-21 13:52:49 +0100

    Started simple arithmetic compiler

--- C/practice/simple_arithmetic_compiler/arithmetic_compiler.c
+++ C/practice/simple_arithmetic_compiler/arithmetic_compiler.c
@@ -2,3 +2,15 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+
+typedef enum {
+    TOKEN_NUMBER,
+    TOKEN_PLUS,
+    TOKEN_MINUS,
+    TOKEN_END
+} TokenType;
+
+typedef struct {
+    TokenType token_type;
+    int value; // Only valid when token_type is TOKEN_NUMBER
+} Token;
