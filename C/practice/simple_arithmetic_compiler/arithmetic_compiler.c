74881af2849a893f3e1b3e5c887371eb9a5d0c6e
Andrei Visan^ <andrei.m.visan@gmail.com>
2025-01-21 15:33:39 +0100

    Started simple arithmetic compiler

--- C/practice/simple_arithmetic_compiler/arithmetic_compiler.c
+++ C/practice/simple_arithmetic_compiler/arithmetic_compiler.c
@@ -14,3 +14,33 @@ typedef struct {
     TokenType token_type;
     int value; // Only valid when token_type is TOKEN_NUMBER
 } Token;
+
+Token *tokenize(const char *input) {
+    Token *tokens = malloc(256 * sizeof(Token));
+    int index = 0;
+    while (input) {
+        if (isdigit(input)) {
+            tokens[index].token_type = TOKEN_NUMBER;
+            tokens[index].value = strtol(input, &input, 10);
+            index++;
+        } else {
+            switch(*input) {
+                case '+':
+                    tokens[index].token_type = TOKEN_PLUS;
+                    input++;
+                    index++;
+                    break;
+                case '-':
+                    tokens[index].token_type = TOKEN_MINUS;
+                    input++;
+                    index++;
+                    break;
+                default:
+                    input++;
+                    break; 
+            }
+        }
+    }
+    tokens[index].token_type = TOKEN_END;
+    return tokens;
+}
