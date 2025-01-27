06fe63b0e21b418d7769259e17a4c0ebcfb1675f
Andrei Visan^ <andrei.m.visan@gmail.com>
2025-01-27 17:43:41 +0100

    Progress on the LLA Zero2Hero C final project

--- 2.C/practice/lla_file_db/src/main.c
+++ 2.C/practice/lla_file_db/src/main.c
@@ -19,6 +19,7 @@ int main(int argc, char *argv[]) {
     //getopt case - what we get from commandline
     int c;
     int dbfd = -1;
+    struct dbheader_t *header = NULL;
     while ((c = getopt(argc, argv, "nf:")) != -1) {
         switch (c) {
             case 'n':
@@ -47,12 +48,17 @@ int main(int argc, char *argv[]) {
             printf("Unable to create database file\n");
             return -1;
         }
+        if (create_db_header(dbfd, &header) == STATUS_ERROR) {
+            printf("Failed to create db header\n");
+            return -1;
+        }
     } else {
         dbfd = open_db_file(filepath);
         if (dbfd == STATUS_ERROR) {
             printf("Unable to open database file\n");
             return -1;
         }
+        
     }
 
 
