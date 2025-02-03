081d8da453912cf07286b45596bd76a98ad82a33
Andrei Visan^ <andrei.m.visan@gmail.com>
2025-02-03 16:50:42 +0100

    lla exercise of the day - start

---
+++ 2.C/practice/lla_exercise/string_replace.c
@@ -0,0 +1,22 @@
+/*
+ * ==================
+ *   STRING REPLACE
+ * ==================
+ * Given an ASCII printable buffer buf, replace all instances of 
+ * sub with the substring rep.
+ * 
+ * For example:
+ *   - buf = hey there kid
+ *   - sub = kid
+ *   - rep = other guy
+ * Should Output:
+ *   - hey there other guy
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void repstr(char *buf, char *sub, char *rep) {
+
+}
+
