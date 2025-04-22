2e268b97e3d96d2066b7f459fcbcb718fba7e031
Andrei Visan <andrei.m.visan@gmail.com>
2025-04-22 17:14:04 +0200

    started to learn Rust

---
+++ 5.Rust/learning_projects/hello_cargo/target/debug/deps/hello_cargo-bc06ab510339842e.d
@@ -0,0 +1,5 @@
+/Users/andreivisan/dev/arc2cracked/5.Rust/learning_projects/hello_cargo/target/debug/deps/libhello_cargo-bc06ab510339842e.rmeta: src/main.rs
+
+/Users/andreivisan/dev/arc2cracked/5.Rust/learning_projects/hello_cargo/target/debug/deps/hello_cargo-bc06ab510339842e.d: src/main.rs
+
+src/main.rs:
