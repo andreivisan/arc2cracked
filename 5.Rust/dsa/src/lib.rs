aab6d22b68dc76c9471fe7d417c435b329808dc9
Andrei Visan <andrei.m.visan@gmail.com>
2025-05-08 21:15:48 +0200

    Learning Rust - DSA NOOB

--- 5.Rust/dsa/src/lib.rs
+++ 5.Rust/dsa/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod twosum;
 pub mod valid_parentheses;
+pub mod merge_two_sorted_lists;
--- 5.Rust/dsa/src/merge_two_sorted_lists.rs
+++ 5.Rust/dsa/src/merge_two_sorted_lists.rs
@@ -20,13 +20,14 @@ pub fn merge_two_lists(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>
         (None, Some(l2)) => Some(l2),
         (Some(mut l1), Some(mut l2)) => {
             if l1.val <= l2.val {
-                l1.next = Self::merge_two_lists(l1.next, Some(l2));
+                l1.next = merge_two_lists(l1.next, Some(l2));
                 Some(l1)
             } else {
-                l2.next = Self::merge_two_lists(Some(l1), l2.next);
+                l2.next = merge_two_lists(Some(l1), l2.next);
                 Some(l2)
             }
-        }
+        },
+        (None, None) => None
     }         
 }
 
