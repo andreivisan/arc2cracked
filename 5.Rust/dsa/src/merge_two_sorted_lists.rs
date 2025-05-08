b0c149b7aca4652abe50bc541384ba683cf16d6a
Andrei Visan <andrei.m.visan@gmail.com>
2025-05-08 21:12:03 +0200

    Learning Rust - DSA NOOB

--- 5.Rust/dsa/src/merge_two_sorted_lists.rs
+++ 5.Rust/dsa/src/merge_two_sorted_lists.rs
@@ -18,15 +18,132 @@ pub fn merge_two_lists(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>
     match (list1, list2) {
         (Some(l1), None) => Some(l1),
         (None, Some(l2)) => Some(l2),
-        (Some(l1), Some(l2)) => {
+        (Some(mut l1), Some(mut l2)) => {
             if l1.val <= l2.val {
-                l1 = l1.next;
+                l1.next = Self::merge_two_lists(l1.next, Some(l2));
+                Some(l1)
             } else {
-                let temp = &l1.next
-                l1.next = &l2;
-                l2.next = temp
+                l2.next = Self::merge_two_lists(Some(l1), l2.next);
+                Some(l2)
             }
-        },
-        _ => break;
+        }
     }         
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_merge_two_lists_empty() {
+        // Test merging two empty lists
+        let list1 = None;
+        let list2 = None;
+        assert_eq!(merge_two_lists(list1, list2), None);
+    }
+
+    #[test]
+    fn test_merge_two_lists_one_empty() {
+        // Test merging when one list is empty
+        let list1 = Some(Box::new(ListNode::new(1)));
+        let list2 = None;
+        assert_eq!(merge_two_lists(list1.clone(), list2), list1);
+
+        let list1 = None;
+        let list2 = Some(Box::new(ListNode::new(2)));
+        assert_eq!(merge_two_lists(list1, list2.clone()), list2);
+    }
+
+    #[test]
+    fn test_merge_two_lists_sorted() {
+        // Test merging two sorted lists with distinct values
+        let mut list1 = Some(Box::new(ListNode::new(1)));
+        list1.as_mut().unwrap().next = Some(Box::new(ListNode::new(3)));
+
+        let mut list2 = Some(Box::new(ListNode::new(2)));
+        list2.as_mut().unwrap().next = Some(Box::new(ListNode::new(4)));
+
+        let merged = merge_two_lists(list1, list2);
+
+        let mut expected = Some(Box::new(ListNode::new(1)));
+        expected.as_mut().unwrap().next = Some(Box::new(ListNode::new(2)));
+        expected.as_mut().unwrap().next.as_mut().unwrap().next = Some(Box::new(ListNode::new(3)));
+        expected.as_mut().unwrap().next.as_mut().unwrap().next.as_mut().unwrap().next = Some(Box::new(ListNode::new(4)));
+
+        assert_eq!(merged, expected);
+    }
+
+    #[test]
+    fn test_merge_two_lists_duplicates() {
+        // Test merging lists with duplicate values
+        let mut list1 = Some(Box::new(ListNode::new(1)));
+        list1.as_mut().unwrap().next = Some(Box::new(ListNode::new(2)));
+
+        let mut list2 = Some(Box::new(ListNode::new(2)));
+        list2.as_mut().unwrap().next = Some(Box::new(ListNode::new(3)));
+
+        let merged = merge_two_lists(list1, list2);
+
+        let mut expected = Some(Box::new(ListNode::new(1)));
+        expected.as_mut().unwrap().next = Some(Box::new(ListNode::new(2)));
+        expected.as_mut().unwrap().next.as_mut().unwrap().next = Some(Box::new(ListNode::new(2)));
+        expected.as_mut().unwrap().next.as_mut().unwrap().next.as_mut().unwrap().next = Some(Box::new(ListNode::new(3)));
+
+        assert_eq!(merged, expected);
+    }
+    
+    #[test]
+    fn test_merge_two_lists_different_lengths() {
+        // Test merging lists of different lengths
+        let mut list1 = Some(Box::new(ListNode::new(1)));
+        list1.as_mut().unwrap().next = Some(Box::new(ListNode::new(3)));
+        list1.as_mut().unwrap().next.as_mut().unwrap().next = Some(Box::new(ListNode::new(5)));
+
+        let mut list2 = Some(Box::new(ListNode::new(2)));
+        list2.as_mut().unwrap().next = Some(Box::new(ListNode::new(4)));
+
+        let merged = merge_two_lists(list1, list2);
+
+        let mut expected = Some(Box::new(ListNode::new(1)));
+        expected.as_mut().unwrap().next = Some(Box::new(ListNode::new(2)));
+        expected.as_mut().unwrap().next.as_mut().unwrap().next = Some(Box::new(ListNode::new(3)));
+        expected.as_mut().unwrap().next.as_mut().unwrap().next.as_mut().unwrap().next = Some(Box::new(ListNode::new(4)));
+        expected.as_mut().unwrap().next.as_mut().unwrap().next.as_mut().unwrap().next.as_mut().unwrap().next = Some(Box::new(ListNode::new(5)));
+
+        assert_eq!(merged, expected);
+    }
+    
+    #[test]
+    fn test_merge_two_lists_single_elements() {
+        // Test merging two single-element lists
+        let list1 = Some(Box::new(ListNode::new(1)));
+        let list2 = Some(Box::new(ListNode::new(2)));
+
+        let merged = merge_two_lists(list1, list2);
+
+        let mut expected = Some(Box::new(ListNode::new(1)));
+        expected.as_mut().unwrap().next = Some(Box::new(ListNode::new(2)));
+
+        assert_eq!(merged, expected);
+    }
+    
+    #[test]
+    fn test_merge_two_lists_negative_values() {
+        // Test merging lists with negative values
+        let mut list1 = Some(Box::new(ListNode::new(-3)));
+        list1.as_mut().unwrap().next = Some(Box::new(ListNode::new(1)));
+
+        let mut list2 = Some(Box::new(ListNode::new(-2)));
+        list2.as_mut().unwrap().next = Some(Box::new(ListNode::new(5)));
+
+        let merged = merge_two_lists(list1, list2);
+
+        let mut expected = Some(Box::new(ListNode::new(-3)));
+        expected.as_mut().unwrap().next = Some(Box::new(ListNode::new(-2)));
+        expected.as_mut().unwrap().next.as_mut().unwrap().next = Some(Box::new(ListNode::new(1)));
+        expected.as_mut().unwrap().next.as_mut().unwrap().next.as_mut().unwrap().next = Some(Box::new(ListNode::new(5)));
+
+        assert_eq!(merged, expected);
+    }
+}
+
