d8135f7f50eb88dbcdff9c56da4090c02991525d
Andrei Visan <andrei.m.visan@gmail.com>
2025-05-11 15:05:04 +0200

    Learning Rust - DSA NOOB

--- 5.Rust/rustlings/exercises/quizzes/quiz2.rs
+++ 5.Rust/rustlings/exercises/quizzes/quiz2.rs
@@ -17,17 +17,34 @@
 //   the first element is the string, the second one is the command.
 // - The output element is going to be a vector of strings.
 
-enum Command {
+pub enum Command {
     Uppercase,
     Trim,
     Append(usize),
 }
 
-mod my_module {
+pub mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut result = Vec::new();
+        for (mut string, command) in input {
+           match command {
+               Command::Uppercase => {
+                   result.push(string.to_uppercase());
+               }
+               Command::Trim => {
+                   result.push(string.trim().to_string());
+               }
+               Command::Append(val) => {
+                   string.push_str(&"bar".repeat(val));
+                   result.push(string);
+               }
+           } 
+        }
+        result
+    }
 }
 
 fn main() {
@@ -37,7 +54,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
