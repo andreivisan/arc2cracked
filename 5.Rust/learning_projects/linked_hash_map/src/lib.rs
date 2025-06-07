2b2a0c960dd2f471ff85318dc852e4b79dfb1625
Andrei Visan <andrei.m.visan@gmail.com>
2025-06-07 19:44:36 +0200

    Learning Rust - Linked Hash Map jon gjangset

--- 5.Rust/learning_projects/linked_hash_map/src/lib.rs
+++ 5.Rust/learning_projects/linked_hash_map/src/lib.rs
@@ -5,3 +5,19 @@ struct Bucket<K, V> {
 pub struct HashMap<K, V> {
     buckets: Vec<Bucket<K, V>>,
 }
+
+impl<K, V> HashMap<K, V> {
+    pub fn new() -> Self {
+        HashMap {
+            buckets: Vec::new(),
+        }
+    }
+
+    fn resize(&mut self) {
+        
+    }
+
+    pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+        key.hash() % self.buckets.len();
+    }
+}
