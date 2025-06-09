fbb7020882c44360ac29f721264364b7f012b274
Andrei Visan <andrei.m.visan@gmail.com>
2025-06-09 10:18:23 +0200

    Learning Rust

--- 5.Rust/learning_projects/linked_hash_map/src/lib.rs
+++ 5.Rust/learning_projects/linked_hash_map/src/lib.rs
@@ -1,33 +1,41 @@
-use std::{hash::{BuildHasher, DefaultHasher, Hash, Hasher, RandomState}, usize};
+use std::{
+    hash::{BuildHasher, DefaultHasher, Hash, Hasher, RandomState},
+    mem, usize,
+};
 
 const INITIAL_NBUCKETS: usize = 1;
 
 pub struct HashMap<K, V> {
     buckets: Vec<Vec<(K, V)>>,
+    items: usize,
 }
 
 impl<K, V> HashMap<K, V> {
     pub fn new() -> Self {
         HashMap {
             buckets: Vec::new(),
+            items: 0,
         }
     }
 }
 
-impl<K, V> HashMap<K, V> 
-where 
-    K: Hash + Eq
+impl<K, V> HashMap<K, V>
+where
+    K: Hash + Eq,
 {
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+        if self.buckets.is_empty() || self.items > 3 * self.buckets.len() / 4 {
+            self.resize();
+        }
         let mut hasher = DefaultHasher::new();
         key.hash(&mut hasher);
         let bucket = (hasher.finish() % self.buckets.len() as u64) as usize;
         let bucket = &mut self.buckets[bucket];
-        
+
+        self.items += 1;
         // ekey, evalue -> existing key, value
         for &mut (ref ekey, ref mut evalue) in bucket.iter_mut() {
             if ekey == &key {
-                use std::mem;
                 return Some(mem::replace(evalue, value));
             }
         }
@@ -40,9 +48,20 @@ where
         let target_size = match self.buckets.len() {
             0 => INITIAL_NBUCKETS,
             n => 2 * n,
-        }; 
-    }
+        };
 
+        let mut new_buckets = Vec::with_capacity(target_size);
+        new_buckets.extend((0..target_size).map(|_| Vec::new()));
+
+        for (key, value) in self.buckets.iter_mut().flat_map(|bucket| bucket.drain(..)) {
+            let mut hasher = DefaultHasher::new();
+            key.hash(&mut hasher);
+            let bucket = (hasher.finish() % new_buckets.len() as u64) as usize;
+            new_buckets[bucket].push((key, value));
+        }
+
+        mem::replace(&mut self.buckets, new_buckets);
+    }
 }
 
 #[cfg(test)]
@@ -51,7 +70,7 @@ mod tests {
 
     #[test]
     fn insert() {
-        let map = HashMap::new();
+        let mut map = HashMap::new();
         map.insert("foo", 43);
     }
 }
