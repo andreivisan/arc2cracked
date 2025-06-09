b7471842c64687fcdc62080fcf1b29b7ca46f6a2
Andrei Visan <andrei.m.visan@gmail.com>
2025-06-09 09:50:13 +0200

    Learning Rust

--- 5.Rust/learning_projects/linked_hash_map/src/lib.rs
+++ 5.Rust/learning_projects/linked_hash_map/src/lib.rs
@@ -1,13 +1,9 @@
-use std::hash::{BuildHasher, DefaultHasher, Hash, Hasher, RandomState};
+use std::{hash::{BuildHasher, DefaultHasher, Hash, Hasher, RandomState}, usize};
 
 const INITIAL_NBUCKETS: usize = 1;
 
-struct Bucket<K, V> {
-    items: Vec<(K, V)>,
-}
-
 pub struct HashMap<K, V> {
-    buckets: Vec<Bucket<K, V>>,
+    buckets: Vec<Vec<(K, V)>>,
 }
 
 impl<K, V> HashMap<K, V> {
@@ -19,26 +15,43 @@ impl<K, V> HashMap<K, V> {
 }
 
 impl<K, V> HashMap<K, V> 
-where: K: Hash
+where 
+    K: Hash + Eq
 {
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
         let mut hasher = DefaultHasher::new();
         key.hash(&mut hasher);
-        let bucket = hasher.finish() % self.buckets.len();
+        let bucket = (hasher.finish() % self.buckets.len() as u64) as usize;
         let bucket = &mut self.buckets[bucket];
         
-        if let Some(&mut (ref ekey, ref mut value)) = bucket.iter_mut().find(|&mut (ref ekey, _)| ekey == key) {
-        } else {
-            bucket.push((key, value));
-
+        // ekey, evalue -> existing key, value
+        for &mut (ref ekey, ref mut evalue) in bucket.iter_mut() {
+            if ekey == &key {
+                use std::mem;
+                return Some(mem::replace(evalue, value));
+            }
         }
+
+        bucket.push((key, value));
+        None
     }
 
     fn resize(&mut self) {
         let target_size = match self.buckets.len() {
             0 => INITIAL_NBUCKETS,
-            n => 2 * n.
+            n => 2 * n,
         }; 
     }
 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn insert() {
+        let map = HashMap::new();
+        map.insert("foo", 43);
+    }
 }
