21f84eb7a82be3e9e38502596737f350796f8973
Andrei Visan^ <andrei.m.visan@gmail.com>
2025-07-23 20:33:24 +0200

    Started ChatGPT driven Rust learning - palindrome lib done

--- 5.Rust/learning_projects/palicli/palilib/src/lib.rs
+++ 5.Rust/learning_projects/palicli/palilib/src/lib.rs
@@ -1,6 +1,24 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn is_palindrome(s: &str) -> bool {
+    if s.len() < 2 {
+        return true;
+    }
+    let mut left: usize = 0;
+    let mut right: usize = s.len() - 1;
+    let s_bytes = s.as_bytes();
+    while left < right && left < s.len() {
+        let left_char = s_bytes[left as usize] as char;
+        let right_char = s_bytes[right as usize] as char;
+        if left_char != right_char {
+            return false;
+        }
+        left += 1;
+        right -= 1;
+    }
+
+    return true;
 }
+
+
 
 #[cfg(test)]
 mod tests {
@@ -8,7 +26,11 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let true_result = is_palindrome("madam");
+        assert_eq!(true_result, true);
+        let str_len_3 = is_palindrome("aba");
+        assert_eq!(str_len_3, true);
+        let false_result = is_palindrome("Hello");
+        assert_eq!(false_result, false);
     }
 }
