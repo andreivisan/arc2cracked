cb86f123fc7298b7efefc05798fd5843297fe1ef
Andrei Visan <andrei.m.visan@gmail.com>
2025-11-21 19:01:58 +0100

    Updated docs

--- 5.Rust/leetcode75/merge_strings_alt.rs
+++ 5.Rust/leetcode75/merge_strings_alt.rs
@@ -2,13 +2,17 @@ pub fn merge_alt(word1: String, word2: String) -> String {
     let mut result = String::with_capacity(word1.len() + word2.len());
     let mut iter1 = word1.chars();
     let mut iter2 = word2.chars();
-    for (i, j) in iter1.by_ref().zip(iter2.by_ref()) {
-        println!("i: {}, j: {}", i, j);
-        result.push(i);
-        result.push(j);
+    loop {
+        match (iter1.next(), iter2.next()) {
+            (Some(a), Some(b)) => {
+                result.push(a);
+                result.push(b);
+            }
+            (Some(a), None) => { result.push(a); }
+            (None, Some(b)) => { result.push(b); }
+            (None, None) => { break; }
+        }
     }
-    result.extend(iter1);
-    result.extend(iter2);
     result
 }
 
